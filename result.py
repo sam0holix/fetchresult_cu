3f0feed42e70a5d6d425ea45b8eaa7795f68cadc
sam0holix <43233525+sam0holix@users.noreply.github.com>
2019-01-15 23:33:07 +0530

    Create result.py
    currently only works with B.Sc hons gen major part II

---
+++ result.py
@@ -0,0 +1,15 @@
+import requests
+from bs4 import BeautifulSoup
+
+roll=input("Enter the roll number:  ")
+with requests.Session() as c:
+	url = 'http://wbresults.nic.in/curesult/cures_ba_bsc_p22_gen_hons_maj_18.asp'
+  form_data = {
+					'roll': roll, 
+					'B1': 'Submit'
+				}
+        
+  page=c.post(url, data=form_data, headers={"Referer": url})
+	text=BeautifulSoup(page.text, 'html.parser').get_text()
+	data=text[text.find('Roll'):-664]
+	print(data)
